c699e5704c040e1c5f59041e809b1c55e505c369
argoskenny <argoskenny@gmail.com>
2017-10-24 23:27:45 +0800

    update to Swift 4 50%

--- iOS-example/ViewController.swift
+++ iOS-example/ViewController.swift
@@ -16,24 +16,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // 建立按鈕
-        let position_x = UIScreen.mainScreen().bounds.width / 2 - 100
+        let positionX: CGFloat = UIScreen.main.bounds.width / 2.0 - 100.0
         
-        pushToDetail.setTitle("coderanch.net", forState: UIControlState.Normal)
-        pushToDetail.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        pushToDetail.frame = CGRectMake(position_x, 200, 200, 40)
-        pushToDetail.backgroundColor = UIColor.redColor()
-        pushToDetail.addTarget(self, action: "push:", forControlEvents: .TouchUpInside)
-        self.view.addSubview(pushToDetail)
+        pushToDetail.setTitle("HAPPY SISYPHUS", for: UIControlState.normal)
+        pushToDetail.setTitleColor(UIColor.white, for: UIControlState.normal)
+        pushToDetail.frame = CGRect(x: positionX, y: 200.0, width: 200.0, height: 40.0)
+        pushToDetail.backgroundColor = UIColor.red
+        pushToDetail.addTarget(self, action: #selector(push(sender:)), for: UIControlEvents.touchUpInside)
+        view.addSubview(pushToDetail)
     }
     
-    func push(sender: UIButton!){
-        self.navigationController?.pushViewController(DetailViewController(), animated: true)
+    @objc func push(sender: UIButton!){
+        navigationController?.pushViewController(DetailViewController(), animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
